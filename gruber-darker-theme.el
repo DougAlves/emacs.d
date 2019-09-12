816a2a7c266cb56d3eb3ce639c73f93b72c65971
Douglas Felipe Alves Lima <douglas.felipelima18@gmail.com>
2019-09-12 14:15:48 -0300

    First Commit

---
+++ gruber-darker-theme.el
@@ -0,0 +1,44 @@
+(deftheme gruber-darker
+  "Created 2019-07-28.")
+
+(custom-theme-set-variables
+ 'gruber-darker
+ )
+
+(custom-theme-set-faces
+ 'gruber-darker
+ '(border ((t (:background "#101010" :foreground "#453d41"))))
+ '(cursor ((t (:background "#ffdd33"))))
+ '(fringe ((t (:background "#101010" :foreground "#453d41"))))
+ '(link ((t (:foreground "#96a6c8" :underline t))))
+ '(link-visited ((t (:foreground "#9e95c7" :underline t))))
+ '(match ((t (:background "#52494e"))))
+ '(shadow ((t (:foreground "#52494e"))))
+ '(minibuffer-prompt ((t (:foreground "#96a6c8"))))
+ '(region ((t (:background "#484848" :foreground nil))))
+ '(secondary-selection ((t (:background "#484848" :foreground nil))))
+ '(trailing-whitespace ((t (:foreground "#000000" :background "#f43841"))))
+ '(tooltip ((t (:background "#52494e" :foreground "#ffffff"))))
+ '(custom-state ((t (:foreground "#73c936"))))
+ '(font-lock-builtin-face ((t (:foreground "#ffdd33"))))
+ '(font-lock-comment-face ((t (:foreground "#cc8c3c"))))
+ '(font-lock-comment-delimiter-face ((t (:foreground "#cc8c3c"))))
+ '(font-lock-constant-face ((t (:foreground "#95a99f"))))
+ '(font-lock-doc-face ((t (:foreground "#73c936"))))
+ '(font-lock-function-name-face ((t (:foreground "#96a6c8"))))
+ '(font-lock-keyword-face ((t (:foreground "#ffdd33" :bold t))))
+ '(font-lock-preprocessor-face ((t (:foreground "#95a99f"))))
+ '(font-lock-string-face ((t (:foreground "#73c936"))))
+ '(font-lock-type-face ((t (:foreground "#95a99f"))))
+ '(font-lock-variable-name-face ((t (:foreground "#f4f4ff"))))
+ '(font-lock-warning-face ((t (:foreground "#f43841"))))
+ '(info-xref ((t (:foreground "#96a6c8"))))
+ '(highlight ((t (:background "#282828" :foreground nil))))
+ '(mode-line ((t (:background "#282828" :foreground "#ffffff"))))
+ '(mode-line-buffer-id ((t (:background "#282828" :foreground "#ffffff"))))
+ '(mode-line-inactive ((t (:background "#282828" :foreground "#95a99f"))))
+ '(isearch ((t (:foreground "#000000" :background "#f5f5f5"))))
+ '(isearch-fail ((t (:foreground "#000000" :background "#f43841"))))
+ '(default ((t (:foreground "#e4e4ef" :background "#181818")))))
+
+(provide-theme 'gruber-darker)
---
+++ init.el
@@ -0,0 +1,64 @@
+(setq-default tab-width 4)
+
+;disable backup
+(setq backup-inhibited t)
+;disable auto save
+(setq auto-save-default nil)
+
+(setq inhibit-splash-screen t)
+
+(add-to-list 'load-path "~/.emacs.d/undo-tree")
+(add-to-list 'load-path "~/.emacs.d/evil")
+(add-to-list 'load-path "~/.emacs.d/org-bullets")
+(require 'evil)
+(require 'undo-tree)
+(require 'org-bullets)
+(add-hook 'org-mode-hook (lambda () (org-bullets-mode 1)))
+(evil-mode 1)
+
+(setq inhib-startup-screen t)
+(menu-bar-mode 0)
+(tool-bar-mode 0)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-faces-vector
+   [default default default italic underline success warning error])
+ '(ansi-color-names-vector
+   ["#2d3743" "#ff4242" "#74af68" "#dbdb95" "#34cae2" "#008b8b" "#00ede1" "#e1e1e0"])
+ '(custom-enabled-themes (quote (gruber-darker)))
+ '(custom-safe-themes
+   (quote
+	("5c3d187c7cf75ab371b3fbc38778f58a9463b300c9c71e69044fd916361fce8e" "47ec21abaa6642fefec1b7ace282221574c2dd7ef7715c099af5629926eb4fd7" "dd2346baba899fa7eee2bba4936cfcdf30ca55cdc2df0a1a4c9808320c4d4b22" default)))
+ '(package-selected-packages
+   (quote
+	(evil-visual-mark-mode undo-tree evil markdown-mode haskell-mode gruber-darker-theme goto-chg abyss-theme))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+(require 'package)
+(let* ((no-ssl (and (memq system-type '(windows-nt ms-dos))
+                    (not (gnutls-available-p))))
+       (proto (if no-ssl "http" "https")))
+  (when no-ssl
+    (warn "\
+Your version of Emacs does not support SSL connections,
+which is unsafe because it allows man-in-the-middle attacks.
+There are two things you can do about this warning:
+1. Install an Emacs version that does support SSL and be safe.
+2. Remove this warning from your init file so you won't see it again."))
+  ;; Comment/uncomment these two lines to enable/disable MELPA and MELPA Stable as desired
+  (add-to-list 'package-archives (cons "melpa" (concat proto "://melpa.org/packages/")) t)
+  ;;(add-to-list 'package-archives (cons "melpa-stable" (concat proto "://stable.melpa.org/packages/")) t)
+  (when (< emacs-major-version 24)
+    ;; For important compatibility libraries like cl-lib
+    (add-to-list 'package-archives (cons "gnu" (concat proto "://elpa.gnu.org/packages/")))))
+(package-initialize)
+
+;; Undo tree-path
+
