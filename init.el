7df7120fca50a5c9290ba1817648582fb288562f
Douglas Lima <douglas.felipelima18@gmail.com>
2019-12-04 15:06:24 -0300

    Some thing

--- init.el
+++ init.el
@@ -1,10 +1,9 @@
 (setq-default tab-width 4)
-
+(setq c-basic-offset 4)
 ;disable backup
 (setq backup-inhibited t)
 ;disable auto save
 (setq auto-save-default nil)
-
 (setq inhibit-splash-screen t)
 
 (add-to-list 'load-path "~/.emacs.d/undo-tree")
@@ -13,9 +12,8 @@
 (require 'evil)
 (require 'undo-tree)
 (require 'org-bullets)
+(evil-mode t)
 (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1)))
-(evil-mode 1)
-
 (setq inhib-startup-screen t)
 (menu-bar-mode 0)
 (tool-bar-mode 0)
@@ -31,7 +29,7 @@
  '(custom-enabled-themes (quote (gruber-darker)))
  '(custom-safe-themes
    (quote
-	("5c3d187c7cf75ab371b3fbc38778f58a9463b300c9c71e69044fd916361fce8e" "47ec21abaa6642fefec1b7ace282221574c2dd7ef7715c099af5629926eb4fd7" "dd2346baba899fa7eee2bba4936cfcdf30ca55cdc2df0a1a4c9808320c4d4b22" default)))
+	("bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" "5c3d187c7cf75ab371b3fbc38778f58a9463b300c9c71e69044fd916361fce8e" "47ec21abaa6642fefec1b7ace282221574c2dd7ef7715c099af5629926eb4fd7" "dd2346baba899fa7eee2bba4936cfcdf30ca55cdc2df0a1a4c9808320c4d4b22" default)))
  '(package-selected-packages
    (quote
 	(evil-visual-mark-mode undo-tree evil markdown-mode haskell-mode gruber-darker-theme goto-chg abyss-theme))))
@@ -42,6 +40,7 @@
  ;; If there is more than one, they won't work right.
  )
 (require 'package)
+(set-cursor-color "#ff0000")
 (let* ((no-ssl (and (memq system-type '(windows-nt ms-dos))
                     (not (gnutls-available-p))))
        (proto (if no-ssl "http" "https")))
@@ -59,6 +58,3 @@ There are two things you can do about this warning:
     ;; For important compatibility libraries like cl-lib
     (add-to-list 'package-archives (cons "gnu" (concat proto "://elpa.gnu.org/packages/")))))
 (package-initialize)
-
-;; Undo tree-path
-
